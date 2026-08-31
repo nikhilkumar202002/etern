05f3ecdbd4047575710094fc47937df973375bb0
nikhilkumar202002 <nikhil.designs24@gmail.com>
2026-08-31 19:48:40 +0530

    Refactor create-child-profile page layout; add additional decorative images and improve styling consistency

--- app/create-child-profile/page.tsx
+++ app/create-child-profile/page.tsx
@@ -1,17 +1,17 @@
-import React from 'react'
-import Image from 'next/image'
-import Form from "../components/ui/Form"
+import React from "react";
+import Image from "next/image";
+import Form from "../components/ui/Form";
 
 const Page = () => {
   return (
     <section
       style={{
-        minHeight: '100svh',
-        height: '100svh',
-        backgroundColor: '#41B6F2',
-        display: 'flex',
-        flexDirection: 'column',
-        overflow: 'hidden',
+        minHeight: "100svh",
+        height: "100svh",
+        backgroundColor: "#41B6F2",
+        display: "flex",
+        flexDirection: "column",
+        overflow: "hidden",
       }}
     >
       <header className="create-profile-header app-container">
@@ -25,14 +25,14 @@ const Page = () => {
         />
       </header>
 
-          <div className="form-box">
-          <Form/>
+      <div className="form-box">
+        <Form />
       </div>
 
       <div>
         <div className="app-container">
           <div className="create-profile-copy">
-            <h1 className="create-profile-title" style={{ color: '#fff' }}>
+            <h1 className="create-profile-title" style={{ color: "#fff" }}>
               Create Child&apos;s Profile
             </h1>
             <p className="create-profile-subtitle">
@@ -44,28 +44,107 @@ const Page = () => {
 
       <div>
         <div>
-             <Image
-          src="/characters/tiger.png"
-          alt=""
-          width={737}
-          height={1017}
-          aria-hidden="true"
-          quality={100}
-          style={{
-            width: '300px',
-            height: 'auto',
-            display: 'block',
-            position: 'absolute',
-            zIndex: 0,
-            top: 250,
-            left: 100
-          }}
-        />
+          <Image
+            src="/characters/tiger.png"
+            alt=""
+            width={737}
+            height={1017}
+            aria-hidden="true"
+            quality={100}
+            style={{
+              width: "300px",
+              height: "auto",
+              display: "block",
+              position: "absolute",
+              zIndex: 0,
+              top: 250,
+              left: 100,
+            }}
+          />
+        </div>
+
+        <div>
+          <Image
+            src="/icons/cloud.png"
+            alt=""
+            width={737}
+            height={1017}
+            aria-hidden="true"
+            quality={100}
+            style={{
+              width: "60px",
+              height: "auto",
+              display: "block",
+              position: "absolute",
+              zIndex: 0,
+              top: 135,
+              left: 535,
+            }}
+          />
+        </div>
+
+        <div>
+          <Image
+            src="/icons/star.png"
+            alt=""
+            width={737}
+            height={1017}
+            aria-hidden="true"
+            quality={100}
+            style={{
+              width: "50px",
+              height: "auto",
+              display: "block",
+              position: "absolute",
+              zIndex: 0,
+              top: 100,
+              left: -10,
+            }}
+          />
+        </div>
+
+        <div>
+          <Image
+            src="/icons/open-book.png"
+            alt=""
+            width={737}
+            height={1017}
+            aria-hidden="true"
+            quality={100}
+            style={{
+              width: "100px",
+              height: "auto",
+              display: "block",
+              position: "absolute",
+              zIndex: 0,
+              top: 250,
+              left: 480,
+            }}
+          />
+        </div>
+
+        <div>
+          <Image
+            src="/icons/hat.png"
+            alt=""
+            width={737}
+            height={1017}
+            aria-hidden="true"
+            quality={100}
+            style={{
+              width: "120px",
+              height: "auto",
+              display: "block",
+              position: "absolute",
+              zIndex: 0,
+              top: 60,
+              right: 500,
+            }}
+          />
         </div>
       </div>
 
-      <div style={{ marginTop: 'auto', width: '100%', position: 'relative' }}>
-        
+      <div style={{ marginTop: "auto", width: "100%", position: "relative" }}>
         <Image
           src="/patterns/create-profile-wave.png"
           alt=""
@@ -73,10 +152,10 @@ const Page = () => {
           height={0}
           aria-hidden="true"
           style={{
-            width: '100%',
-            height: 'auto',
-            display: 'block',
-            position: 'absolute',
+            width: "100%",
+            height: "auto",
+            display: "block",
+            position: "absolute",
             zIndex: 3,
             bottom: -320,
           }}
@@ -89,19 +168,17 @@ const Page = () => {
           height={747}
           aria-hidden="true"
           style={{
-            width: '100%',
-            height: 'auto',
-            display: 'block',
-            position: 'absolute',
+            width: "100%",
+            height: "auto",
+            display: "block",
+            position: "absolute",
             zIndex: 10,
             bottom: -170,
           }}
         />
       </div>
-
-  
     </section>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
